f6f3519ec577e61f8ffe51190a4e9ae6dbe9f3da
Campbell Hawkins <campbell@ziba-online.com>
2013-09-09 18:42:35 +1200

    cartpauj-register-captcha

---
+++ wp-content/plugins/cartpauj-register-captcha/cartpauj-register-captcha.php
@@ -0,0 +1,88 @@
+<?php
+/*
+Plugin Name: Cartpauj Register Captcha
+Plugin URI: http://cartpauj.icomnow.com/projects/cartpauj-register-captcha-plugin/
+Description: Adds a captcha form to WordPress registration page to prevent SPAM registrations.
+Version: 1.0.01
+Author: Cartpauj
+Author URI: http://cartpauj.icomnow.com/
+Copyright: 2009-2011, cartpauj
+
+GNU General Public License, Free Software Foundation <http://creativecommons.org/licenses/GPL/2.0/>
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+add_action('register_form', 'crcc_add_to_form', 1000);
+function crcc_add_to_form()
+{
+	include_once("shared.php");
+	include_once("captcha_code.php");
+  
+	$plugin_url = WP_PLUGIN_URL.'/cartpauj-register-captcha/';
+	$crcc_captcha = new CaptchaCode();
+	$crcc_code = crcc_str_encrypt($crcc_captcha->generateCode(6));
+  
+	echo
+			'<p>
+				<label>Enter Code:</label>
+					<img style="width:120px !important; padding-bottom: 10px;" src="'.$plugin_url.'captcha_images.php?width=120&height=40&code='.$crcc_code.'" /><br>
+					<input type="text" name="crcc_security_code" class="do_input" value="" style="margin-left: 135px;" size="20" tabindex="1000"><br/>
+					<input type="hidden" name="crcc_security_check" value="'.$crcc_code.'">
+				
+        
+        <label id="crcc_hp_label">HP<br/>
+          <input type="text" name="crcc_hp" value="" class="input" size="20" tabindex="1001" />
+        </label>
+        
+        <script>
+          document.getElementById("crcc_hp_label").style.display="none";
+        </script>
+			</p>';
+}
+
+add_action('register_post', 'crcc_check_code', 10, 3);
+function crcc_check_code($login, $email, $errors)
+{
+	include_once("shared.php");
+  
+	$crcc_code = crcc_str_decrypt($_POST['crcc_security_check']);
+  
+	if(($crcc_code != $_POST['crcc_security_code']) && (!empty($crcc_code)))
+		$errors->add('crcc_error',__('<strong>ERROR</strong>: The code you entered was incorrect, please try again.'));
+  
+  if(!isset($_POST['crcc_hp']) || !empty($_POST['crcc_hp']))
+    $errors->add('crcc_error2', __('<strong>ERROR</strong>: Fatal error occurred.'));
+}
+
+/************************************************************************************
+* TO REPLACE THE WORDPRESS LOGO ON SIGNUP PAGE: Replace the logo-login.png file in
+* this plugin directory with an image of your logo. Image must be 274x63 pixels
+* and in .png format. Once you have replaced the file, just uncomment the line
+* below this box by removing the '//' in front of add_action. Done!
+************************************************************************************/
+//add_action('login_head', 'crcc_login_head');
+function crcc_login_head()
+{
+	$plugin_url = WP_PLUGIN_URL.'/cartpauj-register-captcha/';
+  
+	echo
+			'<style type="text/css">
+				h1 a
+				{
+					background:url("'.$plugin_url.'logo-login.png") no-repeat scroll center top transparent !important;
+				}
+			</style>';
+}
+?>
---
+++ wp-content/plugins/cartpauj-register-captcha/readme.txt
@@ -0,0 +1,51 @@
+=== Cartpauj Register Captcha ===
+Contributors: cartpauj
+Donate link: http://cartpauj.icomnow.com/donate/
+Tags: captcha, register, registration, register captcha, user captcha, registration captcha, captcha form
+Requires at least: 3.0
+Tested up to: 3.6
+Stable tag: 1.0.01
+
+Cartpauj Register Captcha does one simple task, and does it well. It prevents SPAM signups through WordPress's default registration form.
+
+== Description ==
+Cartpauj Register Captcha does one simple task, and does it well. It prevents SPAM signups through WordPress's default registration form. There are no settings to configure or get confused with. Just activate and watch those SPAM sign-ups fade away! Cartpauj Register Captcha blocks bots by using two different methods to deter bots:
+* Method 1, it adds a CAPTCHA code which can only be read by humans.
+* Method 2, it adds a field which is only visible to bots -- bots attempt to fill out all fields when they SPAM forms -- so if we detect input in the hidden field, we know it was a bot, and the signup is rejected.
+
+= Donate =
+[If you like this plugin please consider donating](http://cartpauj.icomnow.com/donate)
+
+= Features =
+* Adds CAPTCHA to the WordPress register sign-up form.
+* Adds hidden honeypot input to further deter bots.
+* NO settings or configurations to deal with.
+* Can replace the default WordPress logo on the login/register form (see note near bottom of "cartpauj-register-captcha.php" file for the how to.
+
+== Installation ==
+1. Upload 'cartpauj-register-captcha.zip' to the '/wp-content/plugins/' directory.
+
+2. Activate the plugin through the 'Plugins' menu in WordPress.
+
+3. Done!
+
+If you have any issues please post something here:
+[Cartpauj Support Forums](http://cartpauj.icomnow.com/forum "Cartpauj Support Forums")
+
+== Note ==
+Cartpauj Register Captcha should work with older versions of WordPress as well but was not tested with anything older than 3.0
+
+== Frequently Asked Questions ==
+n/a
+
+== Upgrade Notice ==
+n/a
+
+== Changelog ==
+= 1.0.01 =
+* Added honeypot protection to further deter bot signups
+= 1.0.00 =
+* Initial release
+
+== Screenshots ==
+none
