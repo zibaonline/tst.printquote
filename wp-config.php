09a5acd85485945e5563ed8b2f6ad37ee6ffea22
Risan Bagja <risanbagja@gmail.com>
2013-09-17 16:18:30 +0700

    Modify wp-config.php to match my local machine's settings

--- wp-config.php
+++ wp-config.php
@@ -19,10 +19,10 @@
 define('DB_NAME', 'printquotedb');
 
 /** MySQL database username */
-define('DB_USER', 'camzibahawk2');
+define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'p5In7[ou73');
+define('DB_PASSWORD', '');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
